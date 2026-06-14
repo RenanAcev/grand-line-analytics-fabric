7ab40fa8a43ad7442491b08fd8153535bd16e5a2
RenanAcev <86937438+RenanAcev@users.noreply.github.com>
2026-06-14 16:21:39 -0600

    Committing 2 items from workspace 3dfb0c31-32fd-4d32-8709-91dea585a17f

---
+++ crew_dim.Notebook/notebook-content.py
@@ -0,0 +1,35 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "6940c3c8-dda0-4a36-b5c5-981e73d670b9",
+# META       "default_lakehouse_name": "OnePieceLakehouse",
+# META       "default_lakehouse_workspace_id": "3dfb0c31-32fd-4d32-8709-91dea585a17f",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "6940c3c8-dda0-4a36-b5c5-981e73d670b9"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+df_silver = spark.table("silver_characters")
+df_silver.printSchema()
+display(df_silver.limit(10))
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
